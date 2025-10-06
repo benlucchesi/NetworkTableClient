f91728bed93399d80173a3c299b0f4d0edd01fa3
Ben Lucchesi <benlucchesi@gmail.com>
2025-10-05 19:09:55 -0700

    simple client

--- Java/src/main/java/Program.java
+++ Java/src/main/java/Program.java
@@ -1,6 +1,10 @@
 
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTablesJNI;
+import edu.wpi.first.networktables.StringPublisher;
+import edu.wpi.first.networktables.StringSubscriber;
+import edu.wpi.first.networktables.StringTopic;
 import edu.wpi.first.util.CombinedRuntimeLoader;
 
 import java.io.IOException;
@@ -26,7 +30,23 @@ public class Program {
 
         CombinedRuntimeLoader.loadLibraries(Program.class, "wpiutiljni", "wpimathjni", "ntcorejni", Core.NATIVE_LIBRARY_NAME, "cscorejni");
 
-        var inst = NetworkTableInstance.getDefault();
-        Mat m = new Mat();
+        NetworkTableInstance networkTable = NetworkTableInstance.getDefault();
+
+        networkTable.startClient4("simple client");
+        networkTable.setServerTeam(6352);
+        // networkTable.setServer("10.63.52.129");
+        networkTable.startDSClient();
+
+        NetworkTable table = networkTable.getTable("datatable");
+        StringTopic myStringTopic = table.getStringTopic("myStringTopic");
+        StringPublisher myStringPublisher = myStringTopic.publish();
+
+        myStringPublisher.set("Hello, robot");
+        System.out.println("sent value...");
+
+        StringTopic limelight = networkTable.getStringTopic("/SmartDashboard/limelight_Interface");
+        StringSubscriber limeLightSubscriber = limelight.subscribe("not set");
+
+        System.out.println(limeLightSubscriber.get());
     }
 }
